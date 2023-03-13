7175bfd2b30e23d4268869d94192fec0d0015b23
Chuck Chau <chuck.chau@pluto.tv>
2023-03-13 11:17:47 -0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/volkstrader/girlfriend
+
+go 1.20
---
+++ profile.go
@@ -0,0 +1,11 @@
+package girlfriend
+
+const old = 25
+
+type Profile struct {
+	Age int
+}
+
+func (v Profile) Breakup() bool {
+	return v.Age >= old
+}
